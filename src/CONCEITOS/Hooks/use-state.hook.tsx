0c476d87aa28505ffcd5c628c46880440cc42286
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2023-06-08 17:57:03 -0300

    iniciando o estudo sobre o conceito das coisas no React

---
+++ src/CONCEITOS/Hooks/use-state.hook.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+/**
+ * 
+ * O uso de useState deve ser SEMPRE utilizado no inicio do componente,
+ * não deve ser utilizado dentro de funções/condições dentre outros, para mais infos:
+ * https://react.dev/learn/state-a-components-memory CTRL+F "Pitfall"
+ * 
+ * CONVENÇÃO: const [something, setSomething]
+ */
+
+function AprendizadoUseState() {
+
+  let [count, setCount] = useState<number>(0);
+
+  function addCount() {
+    setCount(count + 1);
+  }
+
+  function lessCount() {
+    setCount(count - 1);
+  }
+
+  return (
+    <>
+      <button onClick={addCount}>aumentar os numeros</button>
+      <span>{ count }</span>
+      <button onClick={lessCount}>diminuir os numeros</button>
+    </>
+  );
+}
+
+export default AprendizadoUseState;
